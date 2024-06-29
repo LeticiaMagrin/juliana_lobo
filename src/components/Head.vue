2cc508cfd8c1e76efd7f0908ce6a596bbc439981
Leticia Magrin <leticiamagrin@outlook.com>
2024-06-29 19:27:23 -0300

    iniciando projeto

---
+++ src/components/Head.vue
@@ -0,0 +1,38 @@
+<script setup>
+
+
+
+</script>
+
+
+
+<template>
+    <container fluid>
+     <nav class="navbar">
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <a class="navbar-brand" href="#">Navbar</a>
+
+      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#">Disabled</a>
+          </li>
+          
+        </ul>
+      </div>
+    </nav>     
+    </container>
+
+</template>
+
+<style scoped>
+ 
+</style>
